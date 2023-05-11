af99a3c61225af3535939cb7feed254f6d8145d8
Chansu Kim <ckstn0777@naver.com>
2023-05-11 18:17:18 +0900

    fix: Observer Pattern update

--- ObserverPattern/observer.test.ts
+++ ObserverPattern/observer.test.ts
@@ -1,4 +1,5 @@
 import CurrentConditionsDisplay from './Display/CurrentConditionsDisplay'
+import StatisticsDisplay from './Display/StatisticsDisplay'
 import WeatherStation from './WeatherStation'
 
 test('Test Observer Pattern - WeatherStation Display Simulator', () => {
@@ -6,6 +7,8 @@ test('Test Observer Pattern - WeatherStation Display Simulator', () => {
 
   // 디스플레이(옵저버, 구독자) 등록. 지금은 귀찮아서 한개만 등록했지만 이런식으로 디스플레이를 확장시킬 수 있음.
   const currentConditionsDisplay = new CurrentConditionsDisplay(weatherStation)
+  const statisticsDisplay = new StatisticsDisplay(weatherStation)
+
   weatherStation.setMeasurements(7, 55, 30.4)
 
   // 결과:
